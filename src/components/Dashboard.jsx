c6ed121a7f6cc06ba9afc8475685d58c681a064a
Prateek-saraswat <saraswatprateek0@gmail.com>
2025-12-24 17:27:23 +0530

    frontend dashboard error fixed

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,7 +8,7 @@ import './Dashboard.css';
 import Overview from './dashboard/Overview2.jsx';
 import Projects from './dashboard/Projects';
 import Tasks from './dashboard/Tasks';
-import TimeTracking from './dashboard/TimeTracking';
+import TimeTracking from './dashboard/Timetracking.jsx';
 import Team from './dashboard/Team';
 import Reports from './dashboard/Reports';
 import Settings from './dashboard/Settings';
